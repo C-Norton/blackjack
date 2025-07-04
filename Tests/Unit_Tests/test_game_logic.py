c9b86c9e891e11368691a196f42b984b96355e97
Channing Norton <cnorton@pc-solutions.it>
2025-07-04 10:16:11 -0400

    finished test refactoring

--- Tests/Unit_Tests/test_game_logic.py
+++ Tests/Unit_Tests/test_game_logic.py
@@ -11,7 +11,7 @@ import collections
 import pytest
 
 import Blackjack
-from Blackjack import card
+from Blackjack import card, game
 
 
 class TestGameLogic:
@@ -24,9 +24,11 @@ class TestGameLogic:
         # TODO: Add your teardown code here
 
     @pytest.fixture
-    def method_setup(self, request):
+    def method_setup(self, request, mocker):
         print(f"Setting up method: {request.function.__name__}")
-        # TODO: Add your setup code here
+        self.fake_input = mocker.patch("builtins.input")
+        self.fake_print = mocker.patch("builtins.print")
+        self.deck = collections.deque()
         yield
         print(f"Tearing down method: {request.function.__name__}")
         # TODO: Add your teardown code here
@@ -37,3 +39,73 @@ class TestGameLogic:
         assert type(deck) is collections.deque
         assert len(deck) == 52
         assert type(deck.popleft()) is card.Card
+
+    def test_game_logic_invalid_inputs(self, class_setup, method_setup):
+        """
+        This function tests several full games.
+
+        Game Scenarios:
+            1:
+                Player bankroll 100
+                Player antes 150
+                Result: Insufficient funds
+            2:
+                Player bankroll 100
+                Player antes 100
+                Dealer Cards: 7, 8
+                Player cards: 10, 4
+                Player Splits
+                Result: Invalid Split
+            3:
+                Player bankroll 100
+                Player antes 100
+                Dealer Cards: 10, jack
+                Player cards: ace, 8
+                Player doubles down
+                Result: Insufficient funds
+
+        :param fake_print_fake_input:
+        :return:
+        """
+
+    # todo: I added DI for the deck in new_hand. Update tests of main menu, and main menu itself, appropriately
+    def test_game_logic_happy_path(self, class_setup, method_setup):
+        """
+
+        Happy path should do the following
+
+        accept an ante
+        create a dealer
+        deal 2 cards to dealer
+        deal 2 cards to player
+        Loop
+            Accept a player move
+            Accept a dealer move
+        reveal
+        resolve
+        update player
+        update stats
+        This function tests several full games.
+
+        Game Scenarios:
+            1:
+                Player bankroll 100
+                Player antes 100
+                Dealer Cards: 7, 8
+                Player cards: 10, 4
+                Dealer hit: 5
+                Player hit: Jack
+                Result: Player Loss
+            2:
+                Player bankroll 200
+                Player antes 100
+                Dealer Cards: 10, jack
+                Player cards: ace, 8
+                Player doubles down
+                Result: Player loss
+
+
+
+        :param fake_print_fake_input:
+        :return:
+        """
