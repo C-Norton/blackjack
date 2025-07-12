78735fee3d84a8111827646b9508abf69a056e2e
Channing Norton <24469058+C-Norton@users.noreply.github.com>
2025-07-12 13:43:45 -0400

    Update game_participant.py

--- Blackjack/game_participant.py
+++ Blackjack/game_participant.py
@@ -1,5 +1,6 @@
 from .result import Result
+from collections import deque
 
 class game_participant:
-    def take_turn(self, deck) -> Result:
+    def take_turn(self, deck:collections.deque) -> Result:
         pass
